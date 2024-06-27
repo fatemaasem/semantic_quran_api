f395292cbd86399fc9df9e9b2b230cfa82761d4d
fatima asem <you@example.com>
2024-06-27 23:12:31 +0200

    third

--- inc/connection.php
+++ inc/connection.php
@@ -1,20 +1,20 @@
 <?php
 
-// $conn=mysqli_connect('localhost','root','','quran_project');
+$conn=mysqli_connect('localhost','root','','quran_project');
 
-$servername = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
-$dbname = getenv('DB_NAME');
+// $servername = getenv('DB_HOST');
+// $username = getenv('DB_USER');
+// $password = getenv('DB_PASS');
+// $dbname = getenv('DB_NAME');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
 
-header('Content-Type: application/json');
-header('Access-Allow-Origin: *');
+// header('Content-Type: application/json');
+// header('Access-Allow-Origin: *');
--- selectAyah.php
+++ selectAyah.php
@@ -3,7 +3,7 @@ require_once "inc/connection.php";
  require_once "inc/function.php";
 if($_SERVER['REQUEST_METHOD']=='GET'){
     if(isset($_GET['id'])){
-        echo "val";
+       
         $id=$_GET['id'];
         $query="select * from surahs join ayahs on  surahs.id=ayahs.surah_id where  ayahs.id=$id";
         $runQuery=mysqli_query($conn,$query);
